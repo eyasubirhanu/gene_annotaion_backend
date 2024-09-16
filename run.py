086c4bd617293aa6c960bed3e805ffd6efbeb4c9
Tesnim19 <tesnimabdi67@gmail.com>
2024-09-16 10:40:31 +0300

    updating requirements and main entry point

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
+import logging
 from app import app
+from flask import Flask
+from config.auth_config import config
+from app.lib.auth_db import init_db
+
+app.config.from_object(config)
+
+logging.basicConfig(level=logging.DEBUG)
+
+with app.app_context():
+    logging.debug("Initializing database...")
+    init_db()
+    logging.debug("Database initialized successfully.")
+
+
 
 if __name__ == '__main__':
-    print("am running")
+    logging.debug("Starting the application")
     app.run(debug=True, host='0.0.0.0', port=5000)
