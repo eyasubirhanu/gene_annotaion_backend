4bf1bb7e2b467ec9ae3abcdfa52c0e52adab905c
BlightG <birhane.gulilat@gmail.com>
2024-10-10 15:30:17 +0300

    minor updates

--- tests/test_Cyphergenerator/test_querygenerator/test_querygenerator.py
+++ tests/test_Cyphergenerator/test_querygenerator/test_querygenerator.py
@@ -1,12 +1,13 @@
 from app.lib.validator import validate_request
 
-# def test_one_node_query(runner, schema, one_query_list):
-    # query = one_query_list['node']
-    # node_map = validate_request(query, schema)
-    # query = runner.query_Generator(query, node_map)
-    # expected_output = one_query_list['expected_output']
-    # assert type(query) is list
-    # assert query == expected_output
+def test_one_node_query(runner, schema, one_query_list):
+    query = one_query_list['node']
+    node_map = validate_request(query, schema)
+    query = runner.query_Generator(query, node_map)
+    expected_output = one_query_list['expected_output']
+    assert type(query) is list
+    assert type()
+    assert query == expected_output
 
 def test_multiple_node_query(runner, schema, multiple_query_list):
     query = multiple_query_list['node']
